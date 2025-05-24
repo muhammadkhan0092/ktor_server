0a199c4bdd059a94decdd4c296a281d81898b89f
muhammadkhan0092 <muhammadkhancs0092@gmail.com>
2025-05-24 23:11:55 +0500

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "1.9.10"
+    application
+}
+
+group = "com.example"
+version = "0.0.1"
+
+application {
+    mainClass.set("com.example.ApplicationKt")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core:2.3.4")
+    implementation("io.ktor:ktor-server-netty:2.3.4")
+    implementation("ch.qos.logback:logback-classic:1.4.11")
+}
